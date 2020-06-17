b2b1c72b2be575014611e53d6ce029f56409ddec
FELIX-imbetpy-choi <imbetpy2025@naver.com>
2020-06-17 17:16:01 +0900

    springboot Basic setting
    return hello

---
+++ src/main/kotlin/com/felix/test/subproject/web/HelloController.kt
@@ -0,0 +1,15 @@
+package com.felix.test.subproject.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+    @GetMapping("/hello")
+    fun blog(model: Model): String {
+        model["title"] = "Hello"
+        return "hello"
+    }
+}
---
+++ src/test/kotlin/com/felix/test/subproject/SubprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.felix.test.subproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
